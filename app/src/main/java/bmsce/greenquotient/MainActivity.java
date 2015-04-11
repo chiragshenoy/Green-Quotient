f7ac43cb4c133dfab635707f0c98a5a7c1cfcfa4
chiragshenoy <chiragshenoy@gmail.com>
2015-04-11 18:11:47 +0530

    3rd

--- app/src/main/java/bmsce/greenquotient/MainActivity.java
+++ app/src/main/java/bmsce/greenquotient/MainActivity.java
@@ -1,12 +1,14 @@
 package bmsce.greenquotient;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -22,21 +24,35 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
                 .getMap();
-        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-                .title("Hamburg"));
-        Marker kiel = map.addMarker(new MarkerOptions()
-                .position(KIEL)
-                .title("Kiel")
-                .snippet("Kiel is cool")
-                .icon(BitmapDescriptorFactory
-                        .fromResource(R.drawable.ic_launcher)));
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+
+        LatLng current_position = new LatLng(latitude, longitude);
+
+        Marker home_marker = map.addMarker(new MarkerOptions()
+                .position(current_position)
+                .draggable(true));
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(current_position, 15));
 
 
-        // Move the camera instantly to hamburg with a zoom of 15.
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+//        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+//                .title("Hamburg"));
+//        Marker kiel = map.addMarker(new MarkerOptions()
+//                .position(KIEL)
+//                .title("Kiel")
+//                .snippet("Kiel is cool")
+//                .icon(BitmapDescriptorFactory
+//                        .fromResource(R.drawable.ic_launcher)));
 
-        // Zoom in, animating the camera.
-        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+//        // Move the camera instantly to hamburg with a zoom of 15.
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+//
+//        // Zoom in, animating the camera.
+//        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
     }
 
 
